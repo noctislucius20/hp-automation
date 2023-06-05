bf816fb4ed2bd94de4a5f67f4bc9847638fa3478
audreyzahra <audreyzahra@gmail.com>
2023-06-05 17:08:27 +0700

    modify collector, backup collector with collector copy

--- hp-monitoring/collector copy.py
+++ hp-monitoring/collector copy.py
@@ -7,6 +7,7 @@ import threading
 import telebot
 import os
 import ping3
+import paramiko
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -25,7 +26,38 @@ class Connect:
         client.on_connect = on_connect
         client.connect(os.getenv('MQTT_BROKER'), int(os.getenv('MQTT_PORT')))
         return client
+    
+    def check_ping():
+        try:
+            while True:
+                Collector.cursor.execute("SELECT ip_address, ip_gateway FROM sensors")
+                row = Collector.cursor.fetchall()
 
+                for ip in row:
+                    ip_address = ip[0]
+                    ip_gateway = ip[1]
+                    response_address_time = ping3.ping(ip_address, timeout=5)
+
+                    if response_address_time is None:
+                        response_gateway_time = ping3.ping(ip_gateway, timeout=5)
+
+                        if response_gateway_time is not None:
+                            #kalau Gateway sukses, IP Address mati berarti sensor mati.
+                            print(f"Ping to IP Gateway {ip_gateway} successful. Response time: {response_gateway_time} ms")
+                            message = f"Sensor : {ip_address} \nCode : 500 \nDescription : Sensor Off \nStatus Honeypot : Off \nat {datetime.now()} \nMessage : Tidak berhasil melakukan update pada perangkat dengan alamat IP {ip_address}. Cek apakah perangkat hidup."
+                            return message
+                        
+                        else:
+                            #kalau Gateway gak sukses, IP Address sukses berarti internet mati
+                            print(f"Ping to IP Gateway {ip_gateway} timed out.")
+                            message = f"Sensor : {ip_address} \nCode : 501 \nDescription : Internet Off \nStatus Honeypot : Off \nat {datetime.now()} \nMessage : Tidak berhasil melakukan update pada perangkat dengan alamat IP {ip_address}. Cek apakah internet pada perangkat hidup."
+                            return message
+                
+                time.sleep(300)
+                        
+        except (Exception) as error:
+            print(error)
+    
 
 class Collector(Connect):
     connection = psycopg2.connect(user="postgres",
@@ -36,11 +68,6 @@ class Collector(Connect):
     
     cursor = connection.cursor()
     print('Connected to PostgreSQL')
-
-
-    def get_chat_id(chat_id):
-        global message_chat_id
-        message_chat_id = chat_id
 
     def subscribe(client: mqtt):     
         def on_message(client, userdata, msg):
@@ -53,6 +80,7 @@ class Collector(Connect):
                 
                 Collector.insert_data_details()
                 alert_message = Collector.add_history()
+                Regulation.check_honeypot()
 
             except (Exception, psycopg2.Error) as error:
                 print("Failed to insert record into sensor_details / honeypot_details table {}".format(error))
@@ -61,7 +89,7 @@ class Collector(Connect):
         client.on_message = on_message
 
     def insert_data_details():
-        ip_address = data['ip_address'][1]
+        ip_address = data['ip_address'][0]
         id_sensor = 0
 
         if "id_raspi" in data:
@@ -109,7 +137,7 @@ class Collector(Connect):
 
     def add_history():
         try:
-            ip_address = data['ip_address'][1]
+            ip_address = data['ip_address'][0]
             Collector.cursor.execute("SELECT COUNT(*) FROM honeypot_details")
             result = Collector.cursor.fetchone()
             global alert_array
@@ -150,8 +178,10 @@ class Collector(Connect):
                             Collector.connection.commit()
                             print(Collector.cursor.rowcount, f"Record inserted successfully into history table : {datetime.now().isoformat()}")
 
-                            send_alert = f"Sensor : {ip_address} \nCode : 400 \nDescription : Honeypot Off \nStatus Honeypot : {honeypot_status} \nat {current_row_timestamp}"
-                            alert_array.append(send_alert)
+                            # send_alert = f"Sensor : {ip_address} \nCode : 400 \nDescription : Honeypot Off \nStatus Honeypot : {honeypot_status} \nat {current_row_timestamp}"
+                            # alert_array.append(send_alert)
+
+                            # Regulation.check_honeypot(ip_address, hp)
 
                         else:
                             honeypot_status = f"{hp.capitalize()} is {current_row_state}"
@@ -214,8 +244,89 @@ class Collector(Connect):
 
         except (Exception, psycopg2.Error) as error:
             print("Failed to delete record into sensor_details / honeypot_details table {}".format(error))
-            row = Collector.cursor.fetchone()
-            print(row)
+
+
+class Regulation(Collector):
+    #regulasi ketika honeypot mati
+    def check_honeypot():
+        try:
+            ip_address = data['ip_address'][0]
+            Collector.cursor.execute("SELECT COUNT(*) FROM history")
+            result = Collector.cursor.fetchone()
+
+            if result[0] > 0:
+                honeypot = ['dionaea', 'honeytrap', 'gridpot', 'cowrie', 'elasticpot', 'rdpy']
+
+                for hp in honeypot:
+                    query = f"SELECT h.honeypot_status FROM history h JOIN honeypot_sensor hs ON h.honeypot_sensor_id = hs.id JOIN honeypots hp ON hs.honeypot_id = hp.id JOIN sensors s ON hs.sensor_id = s.id WHERE hp.name = '{hp}' AND s.ip_address = '{ip_address}' ORDER BY h.created_at DESC LIMIT 1;"
+                    Collector.cursor.execute(query)
+                    row = Collector.cursor.fetchall()
+
+                    if row:
+                        status = row[0][0]
+                        if status == f'{hp.capitalize()} is Not Running':
+                            #cek honeypot apakah sudah ter-deploy
+                            Collector.cursor.execute(f"SELECT * FROM honeypot_sensor hs JOIN honeypots h ON hs.honeypot_id = h.id JOIN sensors s ON hs.sensor_id = s.id WHERE h.name = '{hp}' AND s.ip_address = '{ip_address}'")
+                            row_deploy = Collector.cursor.fetchall()
+
+                            # SSH ke sensor
+                            if row_deploy:
+                                print(f"Honeypot {hp} deployed on IP Address {ip_address}")
+
+                                client = paramiko.SSHClient()
+                                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+                                hostname = ip_address
+                                port = 22888
+                                username = 'audrey'
+                                password = '050501'
+                                # private_key_path = '/home/audrey/.ssh/id_rsa'
+                                # private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+
+                                client.connect(hostname, port=port, username=username, password=password)
+                                command = ''
+
+                                if hp == 'dionaea':
+                                    command = 'sudo -S docker run --rm -it -p 21:21 -p 42:42 -p 69:69/udp -p 80:80 -p 135:135 -p 443:443 -p 445:445 -p 1433:1433 -p 1723:1723 -p 1883:1883 -p 1900:1900/udp -p 3306:3306 -p 5060:5060 -p 5060:5060/udp -p 5061:5061 -p 11211:11211 -v dionaea:/opt/dionaea -d isif/dionaea:dionaea_hp'
+
+                                if hp == 'honeytrap':
+                                    command = 'sudo -S bash dockerize.sh && sudo docker run -it -p 2222:2222 -p 8545:8545 -p 5900:5900 -p 25:25 -p 5037:5037 -p 631:631 -p 389:389 -p 6379:6379 -v honeytrap:/home -d honeytrap_test:latest'
+
+                                if hp == 'gridpot':
+                                    command = "sudo -S docker run -it -p 102:102 -p 8000:80 -p 161:161 -p 502:502 -d -v gridpot:/gridpot isif/gridpot:gridpot_hp /bin/bash -c 'cd gridpot; gridlabd -D run_realtime=1 --server ./gridpot/gridlabd/3.1/models/IEEE_13_Node_With_Houses.glm; conpot -t gridpot'"
+
+                                if hp == 'cowrie':
+                                    command = 'sudo -S docker run -p 22:2222/tcp -p 23:2223/tcp -v cowrie-etc:/cowrie/cowrie-git/etc -v cowrie-var:/cowrie/cowrie-git/var -d --cap-drop=ALL --read-only isif/cowrie:cowrie_hp'
+
+                                if hp == 'elasticpot':
+                                    command = "sudo -S docker run -it -p 9200:9200/tcp -v elasticpot:/elasticpot/log -d isif/elasticpot:elasticpot_hp /bin/sh -c 'cd elasticpot; python3 elasticpot.py'"
+
+                                if hp == 'rdpy':
+                                    command = "sudo -S docker run -it -p 3389:3389 -v rdpy:/var/log -d isif/rdpy:rdpy_hp /bin/sh -c 'python /rdpy/bin/rdpy-rdphoneypot.py -l 3389 /rdpy/bin/1 >> /var/log/rdpy.log'"
+
+                                sudo_password = '050501'
+                                stdin, stdout, stderr = client.exec_command(command)
+                                stdin.write(sudo_password + '\n')
+                                stdin.flush()
+
+                                stdout_output = stdout.read().decode().strip()
+                                stderr_output = stderr.read().decode().strip()
+
+                                # Check if there is any output
+                                if stdout_output:
+                                    print(f"Standard Output: {stdout_output}")
+                                    print(f"{hp} is successfully running now on sensor {ip_address}")
+
+                                if stderr_output:
+                                    print(f"Standard Error: {stderr_output}")
+
+                                client.close()
+
+                    else:
+                        print(f"Honeypot {hp} not deployed on IP Address {ip_address}")
+        
+        except (Exception) as error:
+            print(error)
 
 
 class Bot(Collector):
@@ -240,10 +351,15 @@ class Bot(Collector):
                     if len(alert_message) != 0:
                         for status in alert_message:
                             Bot.bot.send_message(chat_id=message.chat.id, text=status)
-                            print(f'Sent message to bot telegram at {datetime.now()}')
+                            print(f'Sent message to bot telegram on Honeypot running at {datetime.now()}')
                         alert_message = []
                     else:
                         pass
+
+                if Connect.check_ping():
+                    Bot.bot.send_message(chat_id=message.chat.id, text=Connect.check_ping())
+                    print(f'Sent message to bot telegram on trouble in device at {datetime.now()}')
+                    time.sleep(300)
 
         
     @bot.message_handler(commands=['stop'])
@@ -263,7 +379,7 @@ class Main(Bot):
         while True:
             try:
                 print('Start Polling...')
-                Bot.bot.polling()
+                Bot.bot.polling(interval=0, timeout=30)
             except Exception as e:
                 print("Error : ", e)
 
@@ -277,6 +393,11 @@ class Main(Bot):
         mqtt_thread = threading.Thread(target=Main.run_mqtt_loop)
         mqtt_thread.start()
 
+        ping_thread = threading.Thread(target=Collector.check_ping)
+        ping_thread.start()
+
     
 if __name__ == '__main__':
     Main.run()
+    # Main.run_mqtt_loop()
+    # Regulation.check_honeypot()
